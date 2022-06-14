ca8457213e77053e96935c08af0d1c3c2d5babf8
yixin-code <yixincode@163.com>
2022-06-15 05:56:56 +0800

    cpp对c的类型增强

--- 1cToCpp/test.c
+++ 1cToCpp/test.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
+enum Num
+{
+    zero,
+    one,
+    two,
+    three
+};
+
+void typeEnhance()
+{
+    int *p = malloc(11);
+
+    const int a = 11;
+    int *p2 = &a;
+    *p2 = 111;
+    printf("a = %d\n", a);
+
+    int arr[2][3];
+    int *p3 = arr;
+
+    enum Num num = 1;
+    enum Num num = one;
+
+    int a, b, c = 11;
+    // (a = b) = c; // c 中返回的是常量 相当于 c复制给一段随机值的常量
+}
+
 int main()
 {
-
 
     return 0;
 }
--- 1cToCpp/test.cpp
+++ 1cToCpp/test.cpp
@@ -2,9 +2,51 @@
 
 using namespace std;
 
+enum Num
+{
+    zero,
+    one,
+    two,
+    three
+};
+
+void typeEnhance()
+{
+    int *p = (int *)malloc(11); // 在C++ 中必须强转
+
+    // const int a = 11;
+    // int *p2 = &a; // 这个我记得在VS上是可以的， p2会是一个临时空间地址
+    // *p2 = 111;
+    // cout << "a = " << a << endl;
+
+    int arr[2][3];
+    // int *p = arr;
+    int(*p3)[] = arr; // arr是首元素地址， arr的首元素是int[3] p3 为指向int[3]的指针
+
+    // bool 类型
+    cout << "sizeof(bool) = " << sizeof(bool) << endl;
+
+    // enum Num num = 1; // C++ 中枚举只能赋值选项 
+    enum Num num = one;
+
+    int a, b, c = 11;
+    (a = b) = c; // c++中返回的是引用
+}
+
+void func(long long a)
+{
+    cout << "a = " << a << endl;
+}
+
+void func(int *a)
+{
+    cout << "aaaaaaaaa = " << a << endl;
+}
+
 int main()
 {
-
+    func(NULL); // 0 调用func(long long a)
+    func(nullptr); // 空指针类型 调用func(int *a)
 
     return 0;
 }
