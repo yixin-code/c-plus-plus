e8b73aa369d30539a28028ab7930dbb52cb648d5
yixin-code <yixincode@163.com>
2022-06-16 10:03:12 +0800

    命名空间

--- 3namespace/namespace.cpp
+++ 3namespace/namespace.cpp
@@ -42,10 +42,28 @@ namespace
     int a;
 } // namespace 
 
+namespace C
+{
+    int d;
+} // namespace C
+
 
 int main(int argc, char *argv[])
 {
-    
+    A::a = 111;
+    cout << "A::a" << A::a << endl;
+
+    // int d = 1111;
+    // using namespace A;
+    // cout << "d = " << d << endl; // 1111 会使用就近原则 
+
+    // int d = 1111;
+    // using A::d;
+    // cout << "d = " << d << endl; // 会产生二义性
+
+    // 都有变量d同样会产生二义性
+    // using namespace A;
+    // using namespace C;
 
     return 0;
 }
