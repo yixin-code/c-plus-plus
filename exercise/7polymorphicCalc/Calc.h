a44dd5dd9229518991120de0eda8fe61272af973
yixin-code <yixincode@163.com>
2022-06-21 12:44:29 +0800

    多态

--- exercise/7polymorphicCalc/Calc.h
+++ exercise/7polymorphicCalc/Calc.h
@@ -13,5 +13,6 @@ public:
     void setNum2(int num);
     int getNum() const;
     int getNum2() const;
+    // virtual int result() = 0; // 纯虚函数子类必须重写，父类无需实现，有纯虚函数的类无法实例化对象
     virtual int result();
 };
