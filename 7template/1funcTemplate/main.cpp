3546996819dea362c5e064e8cbf5b5e3075adfcd
yixin-code <yixincode@163.com>
2022-06-22 22:38:24 +0800

    函数模板

---
+++ 7template/1funcTemplate/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template<class T>
+void swap(T &num, T &num2)
+{
+	T temp = num;
+	num = num2;
+	num2 = temp;
+}
+
+int main()
+{
+	int num = 11;
+	int num2 = 22;
+
+	// 自动类型推倒
+	swap(num, num2);
+	std::cout << num << " " << num2 << std::endl;
+
+	// 指定类型
+	swap<int>(num, num2);
+	std::cout << num << " " << num2 << std::endl;
+
+	return 0;
+}
