1d63017944634134108abfa432bb3f49be5107f4
yixin-code <yixincode@163.com>
2022-06-26 17:04:48 +0800

    9exception/

---
+++ 9exception/3interfaceDeclaration/main.cpp
@@ -0,0 +1,17 @@
+// 异常接口声明
+#include <iostream>
+
+using namespace std;
+
+// 只抛出int类型异常,   C++11 不推荐， C++17已抛弃
+// void func() throw(int)
+// {
+//     throw -1;
+// }
+
+int main(int argc, char *argv[])
+{
+    
+
+    return 0;
+}
